31b163373eb1be8b161c15e413ae34fa004f9482
Ranjan Kumar Chanda <ranjanchanda06@gmail.com>
2025-10-01 18:44:45 +0530

    login sidebar navbar frontend part

---
+++ frontend/src/components/UserAvatar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { FaUser, FaUserLock } from "react-icons/fa";
+import { IoLogOutOutline } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+
+const UserAvatar = () => {
+    const [open, setOpen] = useState(false);
+    const [openPassword, setOpenPassword] = useState(false);
+    const { user } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandler = () => {
+        console.log('logout');
+
+    }
+    return (
+        <>
+            <div className=''>
+                <Menu as='div' className="relative inline-block text-left">
+                    <div className="">
+                        <Menu.Button className='w-10 h-10 2xl:w-12 2xl:h-12 items-center justify-center rounded-full bg-blue-600'>
+                            <span className='text-white font-semibold'>
+                                {getInitials(user?.name)}
+                            </span>
+                        </Menu.Button>
+                    </div>
+                </Menu>
+            </div>
+        </>
+    )
+}
+
+export default UserAvatar
